5c418a13404997c910f9dfb175acc697810927a4
Tosu <167279662+about-tosu@users.noreply.github.com>
2024-09-08 19:12:23 +0530

    Update config.py

--- config.py
+++ config.py
@@ -32,7 +32,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/Smaugopp/Voidmusic",
+    "https://github.com/about-tosu/tosu",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
 GIT_TOKEN = getenv(
